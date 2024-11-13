fc572cc20604c643147311d5714cbdc4bd448447
Erluo25 <erluo0225@gmail.com>
2024-11-12 23:24:16 -0500

    Finalize cde

--- utils.py
+++ utils.py
@@ -48,10 +48,12 @@ def print_result(check_result, i, time_usage):
 def demo_result(dataset, methods, methods_exps, plot=True):
     # Loop through all the methods, then for each method, plot all the experiments in a single plot
     # Total of num_methods * num_experiments colors
+    fontsize = 20
     assert len(methods) == len(methods_exps), "Numer of methods and methods experiments do not match"
     if plot:
         plt.figure(figsize=(10, 6))
-        colors = plt.cm.viridis(np.linspace(0, 1, len(methods_exps) * len(methods_exps[0])))
+        #colors = plt.cm.viridis(np.linspace(0, 1, len(methods_exps) * len(methods_exps[0])))
+        colors = ['r', 'b']
     # Loop through all the methods
     line_style = ['-', '--']
     for m, method in enumerate(methods):
@@ -60,10 +62,14 @@ def demo_result(dataset, methods, methods_exps, plot=True):
         results = np.load(results_fn)['results']
         exps = methods_exps[m]
         exp_num = len(exps)
+        # Plot the sum of memory over all experiments
+        if plot:
+            plt_x = np.arange(results.shape[0])
+            sum_mem_result = np.sum(results[:, :, 1], axis=1) 
+            plt.semilogy(plt_x, sum_mem_result, label=f'{method}_mem_sum', 
+                        color=colors[m], linestyle=line_style[m])
         # Loop through all the experiments
         for exp_idx, exp in enumerate(exps):
-            if plot:
-                plt_x = np.arange(results.shape[0])
             max_depth = exp[2]
             total_time = np.sum(results[:, exp_idx, 0])
             num_intersect = np.sum(results[:, exp_idx, 2])
@@ -72,17 +78,15 @@ def demo_result(dataset, methods, methods_exps, plot=True):
             print(f"For problem {results_fn_prefix}, with {exp_idx}-th experiment has the following results:")
             print(f"Total time: {total_time}s, Num Intersect: {num_intersect}, \
                 Num No Intersect: {num_no_intersect}, Num Undecidable: {num_undecidable}, max depth: {max_depth}")
-            if plot:
-                #plt.semilogy(plt_x, results[:, exp_idx, 0], label=f'{method}_time', 
-                #            color=colors[m*exp_num+exp_idx], linestyle='-')
-                plt.semilogy(plt_x, results[:, exp_idx, 1], label=f'{method}_mem', 
-                         color=colors[m*exp_num+exp_idx], linestyle=line_style[m])
     if plot:
         # Label the plot
-        plt.xlabel('Reachable Sets')
-        plt.ylabel('Time and Memory Result')
-        plt.title(f'Time and memory usage comparison for {dataset}')
-        plt.legend()
+        plt.xlabel('Reachable Sets', fontsize=fontsize)
+        plt.xticks(fontsize=fontsize)
+        plt.ylabel('Memory Result', fontsize=fontsize)
+        plt.yticks(fontsize=fontsize)
+        #plt.title(f'Memory usage comparison for {dataset}', fontsize=fontsize)
+        plt.title(f'Memory usage comparison for Laub Loomis System', fontsize=fontsize)
+        plt.legend(fontsize=fontsize)
         plt.show()
     
         
