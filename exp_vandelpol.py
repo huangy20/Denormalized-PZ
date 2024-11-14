f439d5c8604113cbbac24a18547a4090012938aa
Erluo25 <erluo0225@gmail.com>
2024-11-13 21:25:19 -0500

    Change plotting

--- exp_vandelpol.py
+++ exp_vandelpol.py
@@ -2,22 +2,23 @@ from denormalized_pz import *
 from normalized_pz_dfs import *
 
 # All the experiments setup, just copy the one you want and pasted into depz_exps and nmpz_exps below
-depz_exps = [([1, 0], -1.5, 15)]
+#depz_exps = [([1, 0], -1.5, 15)]
 #nmpz_exps = [([1, 0], -1.5, 10)]
 
 dataset = 'VanDelPol'
-#depz_exps =  [([1, 0], -2.0165, 40), ([-1, 0], -2.138, 40), ([0, 1], -2.73, 40), ([0, -1], -2.804, 40)]
-#nmpz_exps = [([0, 1], -2.73, 40)]
+depz_exps =  [([1, 0], -2.0165, 10), ([-1, 0], -2.138, 10), ([0, 1], -2.73, 10), ([0, -1], -2.804, 10)]
+nmpz_exps = [([1, 0], -2.0165, 10), ([-1, 0], -2.138, 10), ([0, 1], -2.73, 10), ([0, -1], -2.804, 10)]
 start_idx = 1
 end_idx = 1349
 
 # Note user is taking charge of setting end_idx to the real_end + 1
 #'''
-exp_depz_intersect(start_idx=start_idx, end_idx=end_idx, exps=depz_exps, dataset=dataset)
+#exp_depz_intersect(start_idx=start_idx, end_idx=end_idx, exps=depz_exps, dataset=dataset)
 #exp_nmpz_intersect_dfs(start_idx=start_idx, end_idx=end_idx, exps=nmpz_exps, dataset=dataset)
 #'''
 # Present the results
-#demo_result(dataset=dataset, methods=['depz', 'nmpz_dfs'], methods_exps=[depz_exps, nmpz_exps], plot=True)
-demo_result(dataset=dataset, methods=['depz'], methods_exps=[depz_exps], plot=True)
+demo_result_mem(dataset=dataset, methods=['depz', 'nmpz_dfs'], methods_exps=[depz_exps, nmpz_exps], plot=True)
+demo_result_time(dataset=dataset, methods=['depz', 'nmpz_dfs'], methods_exps=[depz_exps, nmpz_exps], plot=True)
+#demo_result(dataset=dataset, methods=['depz'], methods_exps=[depz_exps], plot=True)
 #demo_result(dataset=dataset, methods=['nmpz_dfs'], methods_exps=[nmpz_exps], plot=True)
 #'''
--- utils.py
+++ utils.py
@@ -45,7 +45,21 @@ def print_result(check_result, i, time_usage):
     elif check_result is None:
         print(f"Case {i}: Inconclusive result - reached maximum recursion depth." + time_str)
 
-def demo_result(dataset, methods, methods_exps, plot=True, fn=None):
+def get_dataset_formal_name(dataset):
+    d = {
+        'VanDelPol': 'Van der Pol Oscillator',
+        'laubLoomis': 'Laub Loomis System'
+    }
+    return d[dataset]
+
+def get_method_legend_name(method):
+    d = {
+        'depz' : "Denormalized PZ",
+        'nmpz_dfs': "Cyclic PZ"
+    }
+    return d[method]
+
+def demo_result_mem(dataset, methods, methods_exps, plot=True, fn=None):
     # Loop through all the methods, then for each method, plot all the experiments in a single plot
     # Total of num_methods * num_experiments colors
     fontsize = 20
@@ -67,8 +81,8 @@ def demo_result(dataset, methods, methods_exps, plot=True, fn=None):
         # Plot the sum of memory over all experiments
         if plot:
             plt_x = np.arange(results.shape[0])
-            sum_mem_result = np.sum(results[:, :, 1], axis=1) 
-            plt.semilogy(plt_x, sum_mem_result, label=f'{method}_mem_sum', 
+            sum_mem_result = np.sum(results[:, :, 1], axis=1)
+            plt.semilogy(plt_x, sum_mem_result, label=f'{get_method_legend_name(method)}', 
                         color=colors[m], linestyle=line_style[m])
         # Loop through all the experiments
         for exp_idx, exp in enumerate(exps):
@@ -82,12 +96,59 @@ def demo_result(dataset, methods, methods_exps, plot=True, fn=None):
                 Num No Intersect: {num_no_intersect}, Num Undecidable: {num_undecidable}, max depth: {max_depth}")
     if plot:
         # Label the plot
-        plt.xlabel('Reachable Sets', fontsize=fontsize)
+        plt.xlabel('Steps', fontsize=fontsize)
         plt.xticks(fontsize=fontsize)
         plt.ylabel('Memory Result', fontsize=fontsize)
         plt.yticks(fontsize=fontsize)
         #plt.title(f'Memory usage comparison for {dataset}', fontsize=fontsize)
-        plt.title(f'Memory usage comparison for Laub Loomis System', fontsize=fontsize)
+        plt.title(f'Memory usage comparison for {get_dataset_formal_name(dataset)}', fontsize=fontsize)
+        plt.legend(fontsize=fontsize)
+        plt.show()
+
+
+def demo_result_time(dataset, methods, methods_exps, plot=True, fn=None):
+    # Loop through all the methods, then for each method, plot all the experiments in a single plot
+    # Total of num_methods * num_experiments colors
+    fontsize = 20
+    assert len(methods) == len(methods_exps), "Numer of methods and methods experiments do not match"
+    if plot:
+        plt.figure(figsize=(10, 6))
+        #colors = plt.cm.viridis(np.linspace(0, 1, len(methods_exps) * len(methods_exps[0])))
+        colors = ['r', 'b']
+    # Loop through all the methods
+    line_style = ['-', '--']
+    for m, method in enumerate(methods):
+        results_fn_prefix = f"{dataset}_{method}"
+        if fn is None:
+            results_fn = "./Results/" + results_fn_prefix + ".npz"
+        else:
+            results_fn = fn
+        results = np.load(results_fn)['results']
+        exps = methods_exps[m]
+        # Plot the sum of memory over all experiments
+        if plot:
+            plt_x = np.arange(results.shape[0])
+            sum_time_result = np.sum(results[:, :, 0], axis=1)
+            plt.semilogy(plt_x, sum_time_result, label=f'{get_method_legend_name(method)}', 
+                        color=colors[m], linestyle=line_style[m])
+        # Loop through all the experiments
+        for exp_idx, exp in enumerate(exps):
+            max_depth = exp[2]
+            total_time = np.sum(results[:, exp_idx, 0])
+            num_intersect = np.sum(results[:, exp_idx, 2])
+            num_no_intersect = np.sum(results[:, exp_idx, 3])
+            num_undecidable = np.sum(results[:, exp_idx, 4])
+            print(f"For problem {results_fn_prefix}, with {exp_idx}-th experiment has the following results:")
+            print(f"Total time: {total_time}s, Num Intersect: {num_intersect}, \
+                Num No Intersect: {num_no_intersect}, Num Undecidable: {num_undecidable}, max depth: {max_depth}")
+    if plot:
+        # Label the plot
+        plt.xlabel('Steps', fontsize=fontsize)
+        plt.xticks(fontsize=fontsize)
+        plt.ylabel('Computation Time Result', fontsize=fontsize)
+        plt.yticks(fontsize=fontsize)
+        #plt.title(f'Memory usage comparison for {dataset}', fontsize=fontsize)
+        plt.title(f'Computation Time comparison for {get_dataset_formal_name(dataset)}', fontsize=fontsize)
         plt.legend(fontsize=fontsize)
         plt.show()
     
